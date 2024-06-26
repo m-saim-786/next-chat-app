24eaff69d76442114ab3c369e676fb2094538c2f
Hassan Raza <hassanraza01.dev@gmail.com>
2024-06-26 17:03:11 -0500

    Added animated typing indicator

---
+++ components/ui/typingIndicator.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { cn } from "@/lib/utils"
+
+interface TypingIndicationProps {
+  className?: string;
+  animationDuration?: string;
+}
+
+const TypingIndication: React.FC<TypingIndicationProps> = ({
+  className,
+  animationDuration = '1.5s',
+}) => {
+  // Calculate animation delays based on animationDuration prop
+  const delay1 = `${parseInt(animationDuration) * 0.2}s`;
+  const delay2 = `${parseInt(animationDuration) * 0.3}s`;
+  const delay3 = `${parseInt(animationDuration) * 0.4}s`;
+
+  return (
+    <div className="flex items-center h-3">
+      <style>
+        {`
+          @keyframes mercuryTypingAnimation {
+            0% {
+              transform: translateY(0px);
+            }
+            28% {
+              transform: translateY(-5px);
+            }
+            44% {
+              transform: translateY(0px);
+            }
+          }
+
+          .animate-typing1 {
+            animation: mercuryTypingAnimation ${animationDuration} infinite ease-in-out;
+            animation-delay: ${delay1};
+          }
+
+          .animate-typing2 {
+            animation: mercuryTypingAnimation ${animationDuration} infinite ease-in-out;
+            animation-delay: ${delay2};
+          }
+
+          .animate-typing3 {
+            animation: mercuryTypingAnimation ${animationDuration} infinite ease-in-out;
+            animation-delay: ${delay3};
+          }
+        `}
+      </style>
+      <div className="flex items-center space-x-0.5">
+        <div className={cn("bg-black rounded-sm w-1 h-1 animate-typing1", className)}></div>
+        <div className={cn("bg-black rounded-sm w-1 h-1 animate-typing2", className)}></div>
+        <div className={cn("bg-black rounded-sm w-1 h-1 animate-typing3", className)}></div>
+      </div>
+    </div>
+  );
+};
+
+export default TypingIndication;
+
+
